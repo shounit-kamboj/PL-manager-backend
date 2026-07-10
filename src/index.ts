4fb50c2ef6f334b9552c751c49acdb95610ca651
shounit kamboj <skamboj@uvic.ca>
2026-07-10 13:21:30 -0700

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("Hello, welcome to PLManager API!");
+})
+
+    app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+})
