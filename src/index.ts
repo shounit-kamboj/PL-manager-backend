2a73d3f0e528e48ab21926d589eb70aaed5f7fbc
shounit kamboj <skamboj@uvic.ca>
2026-07-10 14:40:28 -0700

    small setup additions

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ const PORT = 8000;
 app.use(express.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("Hello, welcome to PLManager API!");
+  res.send("Hello, welcome to PLManager API!");
 })
 
-    app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 })
