154e9a0cae6e30baffaa495ecbff699f1b938e97
shounit kamboj <skamboj@uvic.ca>
2026-09-18 16:20:54 -0700

    implt site24x7

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI  from "apminsight";
+AgentAPI.config()
+
 import express from 'express';
 import athletesRouter from './routes/athletes';
 import athletesAndCompetitionsRouter from './routes/athletesAndCompetitions';
